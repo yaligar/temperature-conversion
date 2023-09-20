99f6de3e8236095e7b2a6e653f7fd38cef086b7e
yaligar <rashmiyaligar68@gmail.com>
2023-09-20 18:56:25 +0530

    added README file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # temperature-conversion
 React application that allows users to convert temperatures between Celsius and Fahrenheit
+Change the background color of the UI if converted temprature is > 98.6°F or  37°C
+
+## Installation Steps followed to create and run this Repo
+* node version: v14.18.0
+* npm version: 6.14.15
+1. create new repo in git
+2. clone the repo to local [git clone (paste SSH key of the created repo)]
+3. npx create-react-app temp-conv
+4. npm install
+5. git remote add upstream [paste ssh key of main repo]
+6. npm start
+
+### Unit testing
+Implement unit tests using `Jest` library along with `Enzyme`
+1. npm install --save-dev jest
+2. npm i enzyme / npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
+
+To run unit test 
+*  npm test Converter.test.js 
+*  npm run test:coverage
+
+#### working of this project is added in the screen record  in GIT with file name `ScrrenRecordFor_Temp_Conversion.gif`
+
+#### Screenshot for test coverage is added in GIT with file name `Unit_Test_Coverage.PNG`
+
+#### component for Temperature Conversion App `src/components/Converter.js`
+
+#### test file `_test/Converter.test.js`
+
+#### styling added in file `App.css`
--- temp-conv/src/_test/Converter.test.js
+++ temp-conv/src/_test/Converter.test.js
@@ -5,75 +5,76 @@ import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
-import Converter from "../components/Converter"
+import Converter from "../components/Converter";
 import React from "react";
 
 import { shallow, mount } from "enzyme";
 describe("Unit testing for converter", () => {
-    const wrapper = mount(
-        <Converter />
+  const wrapper = mount(<Converter />);
+  test("If swap button is being rendered.", () => {
+    expect(wrapper.find("button").at(0).text()).toBeDefined();
+    expect(wrapper.find("button")).toHaveLength(1);
+    const event = { handleClickSwap: () => {} };
+    jest.spyOn(event, "handleClickSwap");
+    wrapper.find("button").simulate("click", event);
+  });
+  test("to check paragraph tags rendered.", () => {
+    expect(wrapper.find("p")).toHaveLength(2);
+    expect(wrapper.find("p").at(0).text()).toMatch(
+      " Enter Temprature value to Convert"
     );
-    test("If swap button is being rendered.", () => {
-        expect(wrapper.find("button").at(0).text()).toBeDefined();
-        expect(wrapper.find("button")).toHaveLength(1)
-        const event = { handleClickSwap: () => {} };
-        jest.spyOn(event, "handleClickSwap");
-        wrapper.find("button").simulate("click", event);
-    })
-    test("to check paragraph tags rendered.",()=>{
-        expect(wrapper.find("p")).toHaveLength(2)
-        expect(wrapper.find("p").at(0).text()).toMatch(" Enter Temprature value to Convert")
-        expect(wrapper.find("p").at(1).text()).toMatch("Converted Temrature")
-    })
-    test("to check input tag rendered.",()=>{
-        const wrapper = shallow(
-            <Converter />
-        );
-        expect(wrapper.find("input")).toHaveLength(3)
-        const event = { handleRaioChange: () => {} };
-        jest.spyOn(event, "handleRaioChange");
-        wrapper.find("input").at(1).simulate("onMouseDown", event);
-        wrapper.find("input").at(2).simulate("onMouseDown", event);
-    })
+    expect(wrapper.find("p").at(1).text()).toMatch("Converted Temrature");
+  });
+  test("to check input tag rendered.", () => {
+    const wrapper = shallow(<Converter />);
+    expect(wrapper.find("input")).toHaveLength(3);
+    const event = { handleRaioChange: () => {} };
+    jest.spyOn(event, "handleRaioChange");
+    wrapper.find("input").at(1).simulate("onMouseDown", event);
+    wrapper.find("input").at(2).simulate("onMouseDown", event);
+  });
 
-    test("check for label",()=>{
-       expect(wrapper.find("label")).toHaveLength(2)
-    })
+  test("check for label", () => {
+    expect(wrapper.find("label")).toHaveLength(2);
+  });
 
-    test("checking if input for celsious is getting rendered",()=>{
-        const handleRaioChange = jest.fn();
-        const setRadioButton = jest.fn();
-        const radioButton = "Celsius"
-        expect(wrapper.containsMatchingElement(
-            <input
-            className="radioButton"
-            type="radio"
-            name="Celsius"
-            value="Celsius"
-            id="Celsius"
-            checked={radioButton === "Celsius"}
-            onChange={(e) => setRadioButton("Celsius")}
-            onMouseDown={(e) => handleRaioChange("Celsius")}
-          />));
-      })
+  test("checking if input for celsious is getting rendered", () => {
+    const handleRaioChange = jest.fn();
+    const setRadioButton = jest.fn();
+    const radioButton = "Celsius";
+    expect(
+      wrapper.containsMatchingElement(
+        <input
+          className="radioButton"
+          type="radio"
+          name="Celsius"
+          value="Celsius"
+          id="Celsius"
+          checked={radioButton === "Celsius"}
+          onChange={(e) => setRadioButton("Celsius")}
+          onMouseDown={(e) => handleRaioChange("Celsius")}
+        />
+      )
+    );
+  });
 
-      test("checking if input for celsious is getting rendered",()=>{
-        const handleRaioChange = jest.fn();
-        const setRadioButton = jest.fn();
-        const radioButton = "Fahrenheit"
-        expect(wrapper.containsMatchingElement(
-            <input
-            className="radioButton"
-            type="radio"
-            name="Fahrenheit"
-            value="Fahrenheit"
-            id="Fahrenheit"
-            checked={radioButton === "Fahrenheit"}
-            onChange={(e) => setRadioButton("Fahrenheit")}
-            onMouseDown={(e) => handleRaioChange("Fahrenheit")}
-          />));
-      })
-    
-
-    
-})
+  test("checking if input for Fahrenheit is getting rendered", () => {
+    const handleRaioChange = jest.fn();
+    const setRadioButton = jest.fn();
+    const radioButton = "Fahrenheit";
+    expect(
+      wrapper.containsMatchingElement(
+        <input
+          className="radioButton"
+          type="radio"
+          name="Fahrenheit"
+          value="Fahrenheit"
+          id="Fahrenheit"
+          checked={radioButton === "Fahrenheit"}
+          onChange={(e) => setRadioButton("Fahrenheit")}
+          onMouseDown={(e) => handleRaioChange("Fahrenheit")}
+        />
+      )
+    );
+  });
+});
